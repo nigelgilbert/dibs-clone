ddf7c4d5d2203e3d699de9ae4be835198c2ce644
nigelgilbert <nigelcodes@gmail.com>
2025-02-17 01:04:58 -0600

    All tests are passing – `jest.useFakeTimers()` is neat

--- src/ResourceQueue.test.js
+++ src/ResourceQueue.test.js
@@ -2,30 +2,30 @@ const { reserveResource, eventEmitter, Events } = require("./ResourceQueue");
 
 describe("reserveResource", () => {
   // Cleanup listeners to avoid memory leaks
-  afterEach(() => eventEmitter.removeAllListeners())
-  
-  it("Emits a `RESERVED` event when a user reserves a resource", (done) => {
+  // afterEach(() => eventEmitter.removeAllListeners())
+
+  it("Emits a `RESERVED` event when a user reserves a resource", () => {
     const mockHandler = jest.fn();
     eventEmitter.on(Events.RESERVED, mockHandler);
     reserveResource("resource-1", "user", "channel", 100);
     expect(mockHandler).toHaveBeenCalledOnce();
-    setTimeout(() => done(), 200);
   });
 
-  it("Emits a `QUEUED` event when a 2nd user reserves a resource", (done) => {
+  it("Emits a `QUEUED` event when a 2nd user reserves a resource", () => {
     const mockHandler = jest.fn();
     eventEmitter.on(Events.QUEUED, mockHandler);
     reserveResource("resource-2", "first", "channel", 100);
     reserveResource("resource-2", "second", "channel", 100);
     expect(mockHandler).toHaveBeenCalledOnce();
-    setTimeout(() => done(), 200);
   });
 
-  it("Emits a `RELEASED` event when user releases a resource", (done) => {
+  it("Emits a `RELEASED` event when user releases a resource", () => {
+    jest.useFakeTimers();
     const mockHandler = jest.fn();
     eventEmitter.on(Events.RELEASED, mockHandler);
     reserveResource("resource-3", "user", "channel", 100);
+    jest.advanceTimersByTime(1000);
     expect(mockHandler).toHaveBeenCalledOnce();
-    setTimeout(() => done(), 150);
+    jest.useRealTimers();
   });
 });
