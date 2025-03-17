a22352a3f665264556b8a5dd9682ec44a585ea64
nigelgilbert <nigelcodes@gmail.com>
2025-03-16 20:25:37 -0500

    Oops, turn socket mode off now

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const {
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
+  socketMode: false,
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
