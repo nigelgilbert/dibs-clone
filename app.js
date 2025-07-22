9e7e87368ec0971cf7881df9fec33b44988419d9
nigelgilbert <nigelcodes@gmail.com>
2025-07-21 23:21:34 -0500

    I think `user` is better than `user_name` for queue keys It's more secure, it doesn't require PII in memory or server logs

--- app.js
+++ app.js
@@ -35,12 +35,12 @@ app.command(`/${process.env.COMMAND}`, async ({ command, ack, say }) => {
     const duration = await parseDuration(match[2]);
 
     // call dibs
-    reserveResource(match[1], command.user_name, duration);
+    reserveResource(match[1], command.user, duration);
   } else if (command.text.includes("off")) {
     // given "off staging"
     // match[1] = "staging"
     const match = parseOffCommand(command.text);
-    releaseResource(match[1], command.user_name);
+    releaseResource(match[1], command.user);
   }
 });
 
